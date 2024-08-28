a224073290eb0cedbcc8baa1f877bc79c02cdd21
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2024-08-28 15:45:21 +0530

    Update Listener1.java

--- src/java/model/Listener1.java
+++ src/java/model/Listener1.java
@@ -1,5 +1,22 @@
 package model;
 
-public class Listener1 {
-    
+import java.util.ArrayList;
+import java.util.HashMap;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class Listener1 implements ServletContextListener{
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        HashMap<String, User> userMap = new HashMap<String, User>();
+        sce.getServletContext().setAttribute("userMap", userMap);
+        
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
 }
