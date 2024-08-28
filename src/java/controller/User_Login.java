aa08f0a5defe74be6b448e017f103cd8d29105ef
Hansana Madhuranga <153816119+Hansana0613@users.noreply.github.com>
2024-08-28 17:43:54 +0530

    Create User_Login.java
    User_Login.java servlet

---
+++ src/java/controller/User_Login.java
@@ -0,0 +1,24 @@
+package controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+@WebServlet(name = "User_Login", urlPatterns = {"/User_Login"})
+public class User_Login extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String mobile = request.getParameter("mobile");
+        String password = request.getParameter("password");
+        
+        HashMap<String, User> userMap = (HashMap<String, User>) request.getServletContext().getAttribute("userMap");
+        
+    }
+}
--- web/user_login.jsp
+++ web/user_login.jsp
@@ -3,13 +3,14 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>User Login</title>
+        <link rel="stylesheet" href="css/style.css"/>
     </head>
     <body>
 
         <h1>User Login</h1>
         
-        <form action="Login" method="POST">
+        <form action="User_Login" method="POST">
 
             <div class="div-main">
                 <div class="div-sub">Mobile</div>
